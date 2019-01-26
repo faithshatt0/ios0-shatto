3543db73718ef3509eb13622144fcfebcdf2a095
Faith <faith@faiths-mac.local>
2019-01-26 13:37:28 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Faith Shatto on 1/26/19.
+//  Copyright © 2019 Faith Shatto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
